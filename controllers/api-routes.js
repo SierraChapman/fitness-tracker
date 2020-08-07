5c52b1f270a2cbcbdd9a44758e142e253af13ea5
Sierra Chapman <siechap@gmail.com>
2020-08-06 21:11:19 -0700

    write notes specifying API/HTML routes needed

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -4,5 +4,30 @@ const express = require("express");
 // create router
 const router = express.Router();
 
+// add routes
+
+/*
+Routes needed:
+
+GET /api/workouts
+for getting array of workout data
+returns JSON array of workout objects
+
+PUT /api/workouts/:id
+for adding an exercise to a workout
+accepts JSON
+returns JSON
+
+POST /api/workouts
+for creating a workout
+accepts JSON
+resturns JSON
+
+GET /api/workouts/range
+for getting workouts within a range
+returns JSON
+
+*/
+
 // export router
 module.exports = router;
--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -6,8 +6,12 @@ const router = express.Router();
 
 // add routes
 router.get("/", (req, res) => {
-  res.sendFile("./public/index.html");
-})
+  res.sendFile("../public/index.html");
+});
+
+// GET /exercise serves exercise.html
+
+// GET /stats serves stats.html
 
 // export router
 module.exports = router;
