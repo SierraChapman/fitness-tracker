62e271a000cf1896a3560f0402da4353855ac064
Sierra Chapman <siechap@gmail.com>
2020-08-07 20:26:01 -0700

    put exercises nested in workouts to match seeder

--- models/Workout.js
+++ models/Workout.js
@@ -2,6 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const ExerciseSchema = new Schema({
+  // exercise type must be "resistance" or "cardio" and is required
+  type: {
+    type: String,
+    required: true,
+    enum: ["resistance", "cardio"],
+  },
+  // exercise name can be any nonempty string
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  // duration cannot be negative
+  duration: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  // distance, weight, reps, and sets are not required but cannot be negative
+  distance: {
+    type: Number,
+    required: false,
+    min: 0,
+  },
+  weight: {
+    type: Number,
+    required: false,
+    min: 0,
+  },
+  reps: {
+    type: Number,
+    required: false,
+    min: 0,
+  },
+  sets: {
+    type: Number,
+    required: false,
+    min: 0,
+  },
+});
+
 const WorkoutSchema = new Schema({
   // day is a date and is required
   day: {
@@ -9,13 +51,11 @@ const WorkoutSchema = new Schema({
     required: true,
   },
   // exercises is an array of exercise ID's and is required
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise",
+  exercises: {
+      type: [ExerciseSchema],
       required: true,
+      default: []
     }
-  ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
