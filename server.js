3dd1124c7432ae3fdd7e72646cbcb9656066ee7f
Sierra Chapman <siechap@gmail.com>
2020-08-07 21:36:44 -0700

    make seeder and server db connection options match

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static("./public"));
 // connect to database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
