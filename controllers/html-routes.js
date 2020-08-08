7a6c660c59bbf98a76eaf5c9df6289986ea522fa
Sierra Chapman <siechap@gmail.com>
2020-08-07 17:51:13 -0700

    create HTML routes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,17 +1,24 @@
 // import dependencies 
 const express = require("express");
+const path = require("path");
 
 // create router
 const router = express.Router();
 
 // add routes
 router.get("/", (req, res) => {
-  res.sendFile("../public/index.html");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // GET /exercise serves exercise.html
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 // GET /stats serves stats.html
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 // export router
 module.exports = router;
